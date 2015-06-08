67efdce8c10f9bc1c9c33430136bd193a9159ee1
Augusto Soares <gutosoares@outlook.com>
2015-06-07 21:27:18 -0300

    Definição das funções basicas do jogo

---
+++ assets/engine.js
@@ -0,0 +1,29 @@
+// Variáveis do jogo
+var canvas, contexto, ALTURA, LARGURA, frames = 0;
+
+function main() {
+
+}
+
+//monitorar o clique do usuário
+function clique() {
+
+}
+
+// função onde irá "rodar" o jogo
+function roda() {
+
+}
+
+// local onde irá atualizar o status do personagem
+function atualiza() {
+
+}
+
+// desenhar o ambiente e o personagem
+function desenha() {
+
+}
+
+// inicializa o jogo
+main();
