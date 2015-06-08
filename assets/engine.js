be028844e18aad2e9749257e835294b064f1126b
Augusto Soares <gutosoares@outlook.com>
2015-06-07 21:58:27 -0300

    Adicionando o elemento canvas no html

--- assets/engine.js
+++ assets/engine.js
@@ -3,6 +3,25 @@ var canvas, contexto, ALTURA, LARGURA, frames = 0;
 
 function main() {
 
+    // pagando a altura e largura da janela do usuário
+    ALTURA = window.innerHeight;
+    LARGURA = window.innerWidth;
+
+    if(LARGURA >= 500) {
+        LARGURA = 600;
+        ALTURA = 600;
+    }
+
+    // criando o elemento canvas
+    canvas = document.createElement("canvas");
+    canvas.width = LARGURA;
+    canvas.height = ALTURA;
+    canvas.style.border = "1px solid #000";
+
+    // adicionando o elementos canvas no html
+    contexto = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+
 }
 
 //monitorar o clique do usuário
@@ -10,7 +29,7 @@ function clique() {
 
 }
 
-// função onde irá "rodar" o jogo
+// função onde irá "rodar" o jogo, loop do jogo
 function roda() {
 
 }
